64a26088c0f73c2d50197b440bcbbcf6a455c420
Shaheen Shaik <skshaheen0002@gmail.com>
2026-07-27 09:26:28 +0530

    Day-3

--- patterns.py
+++ patterns.py
@@ -61,20 +61,20 @@
 #             print(" ",end=" ")
 #     print()
 
-#pascal's triangle
-""" 
-    1
-   1 1
-  1 2 1
- 1 3 3 1
-1 4 6 4 1 
-"""
-# n=int(input("Enter the num of rows: "))
-# for i in range(n):
-#     print(" " *(n-i-1),end="")
-#     num=1
-#     for j in range(i+1):
-#         print(num,end=" ")
-#         num=num*(i-j)//(j+1)
-#     print()
+# #pascal's triangle
+# """ 
+#     1
+#    1 1
+#   1 2 1
+#  1 3 3 1
+# 1 4 6 4 1 
+# """
 
+# # n=int(input("Enter the num of rows: "))
+# # for i in range(n):
+# #     print(" " *(n-i-1),end="")
+# #     num=1
+# #     for j in range(i+1):
+# #         print(num,end=" ")
+# #         num=num*(i-j)//(j+1)
+# #     print()
